78f03e913741092a050b4da5baedc74a9a03688b
Mar <marpouradressa@gmail.com>
2026-02-11 11:18:20 +0100

    Ex1 done, basic number array and foreach

---
+++ level-01/exercise-01.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/*Crea un array, afegeix-li 5 nombres enters i després mostrals
+ per pantalla d’un en un.*/
+
+$numbers = [6, 6, 10, 35, 17];
+
+foreach ($numbers as $num) {
+echo "- " . $num . "\n";
+}
+
+
+?>
